d993ef61b9633aadff14f8a49b565344457fbe2c
unknown <kristanalgo@gmail.com>
2024-01-29 15:58:30 +0800

    updated index.php

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>BSIS-3E Gitgit</title>
 </head>
 
 <body>
-
+  <p>Hakdog</p>
 </body>
 
 </html>
