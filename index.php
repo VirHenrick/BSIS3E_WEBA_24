7bf229f52f3b7a134ce960416ab5495c93c3fe7d
unknown <kristanalgo@gmail.com>
2024-01-29 16:33:36 +0800

    Bago ito

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>BSIS-3E Gitgit</title>
+  <title>BSIS-3E Githubbing</title>
 </head>
 
 <body>
