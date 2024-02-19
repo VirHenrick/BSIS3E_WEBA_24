a3975f1bcae416c28f86459b8250568bfe2a5e95
Erwin <kristanalgo@gmail.com>
2024-02-19 15:24:18 +0800

    update

---
+++ mvc/app/core/App.php
@@ -0,0 +1,13 @@
+<>php
+
+class App
+{
+
+    public function_construct()
+    {
+        echo '<pre>';
+        print_r(explode("/", trim($_GET['url'], "/")));
+        echo '</pre>';
+        
+    }
+}
