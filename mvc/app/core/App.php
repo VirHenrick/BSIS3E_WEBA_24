37fe2a7009307cbb7ef3a91ef854b140183e5b47
Erwin <kristanalgo@gmail.com>
2024-02-19 15:49:06 +0800

    bago

--- mvc/app/core/App.php
+++ mvc/app/core/App.php
@@ -5,9 +5,18 @@ class App
 
     public function_construct()
     {
+        $url = $this->splitURL();
+
         echo '<pre>';
-        print_r(explode("/", trim($_GET['url'], "/")));
+        print_r();
         echo '</pre>';
-        
+    }
+
+    private function splitURL()
+    {
+        if (isset($_GET['url'])){
+        $url = explode("/", trim($_GET['url'], "/"));
+        return $url;
+        }       
     }
 }
